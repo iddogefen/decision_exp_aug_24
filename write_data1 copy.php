c9bdbf77870703f5743a10887a76530d2c85cf56
iddo gefen <iddo.gefen@gmail.com>
2024-07-16 15:02:34 -0400

    first commit

--- write_data1 copy.php
+++ write_data1 copy.php
@@ -3,9 +3,10 @@
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['filedata'];
 // generate a unique ID for the file, e.g., session-6feu833950202 
-$file = date("Y-m-d-h-i-sa");
+$file = uniqid("session-");
 // the directory "data" must be writable by the server
 $name = "data/{$file}.csv"; 
 // write the file to disk
 file_put_contents($name, $data);
 ?>
+function saveData(name, data){
