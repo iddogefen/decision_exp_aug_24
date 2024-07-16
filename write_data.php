8b27642d3597b1f874800f35070ad948e7a9bcbd
iddo gefen <iddo.gefen@gmail.com>
2024-07-16 14:55:27 -0400

    first commit

--- write_data.php
+++ write_data.php
@@ -1,11 +1,14 @@
 <?php
-// get the data from the POST message
+// Get the data from the POST message
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['filedata'];
-// generate a unique ID for the file, e.g., session-6feu833950202 
-$file = date("Y-m-d-h-i-sa");
-// the directory "data" must be writable by the server
+$subject_id = $post_data['subject_id']; // Extract the subject ID
+
+// Generate a unique filename using subject_id and current date
+$file = "{$subject_id}-" . date("Y-m-d-H-i-s"); 
+// The directory "data" must be writable by the server
 $name = "data/{$file}.csv"; 
-// write the file to disk
+
+// Write the file to disk
 file_put_contents($name, $data);
 ?>
